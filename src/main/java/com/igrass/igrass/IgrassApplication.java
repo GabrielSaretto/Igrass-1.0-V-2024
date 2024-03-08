f9352b2184a7f917382492383943ff83f287d12e
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-03-08 15:12:47 -0300

    first commit

---
+++ src/main/java/com/igrass/igrass/IgrassApplication.java
@@ -0,0 +1,13 @@
+package com.igrass.igrass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgrassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgrassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/igrass/igrass/IgrassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igrass.igrass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgrassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
